aa68c1d58c35f191ce91d8d01c534a03fcbbd88e
Jose Luis <josvarg2014@gmail.com>
2016-06-17 17:52:07 +0200

    suma inplementada

---
+++ src/proyectonuevo/Proyectonuevo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectonuevo;
+
+/**
+ *
+ * @author EPIS
+ */
+public class Proyectonuevo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    float sumar(float a, float b){
+    
+    return a+b;
+    }
+    
+}
